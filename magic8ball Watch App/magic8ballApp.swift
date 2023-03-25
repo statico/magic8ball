61cc9b145dac7d83c8a7bd5781a4fada9b895a28
Ian Langworth <ian@langworth.com>
2023-03-24 22:44:25 -0700

    Initial Commit

---
+++ magic8ball Watch App/magic8ballApp.swift
@@ -0,0 +1,17 @@
+//
+//  magic8ballApp.swift
+//  magic8ball Watch App
+//
+//  Created by Ian on 3/24/23.
+//
+
+import SwiftUI
+
+@main
+struct magic8ball_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ magic8ball/magic8ballApp.swift
@@ -0,0 +1,17 @@
+//
+//  magic8ballApp.swift
+//  magic8ball
+//
+//  Created by Ian on 3/24/23.
+//
+
+import SwiftUI
+
+@main
+struct magic8ballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
