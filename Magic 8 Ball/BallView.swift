109d527e4fcab63419a92cd1ca8d570094b76450
Ian Langworth <ian@langworth.com>
2023-03-26 11:57:06 -0700

    Fix BallView.swift name

--- Magic 8 Ball/BallView.swift
+++ Magic 8 Ball/BallView.swift
@@ -1,5 +1,5 @@
 //
-//  Ball.swift
+//  BallView.swift
 //  Magic 8 Ball
 //
 //  Created by Ian on 3/26/23.
@@ -10,45 +10,36 @@ import SceneKit
 import SwiftUI
 import UIKit
 
-struct Ball: UIViewRepresentable {
+struct BallView: UIViewRepresentable {
   let scene: SCNScene
-  private var ballNode: SCNNode?
+  let childNodeName: String
   
   func makeUIView(context: Context) -> SCNView {
     print("ball makeUIView")
-    let sceneView = SCNView(frame: .zero)
-    sceneView.scene = scene
-    sceneView.allowsCameraControl = true
-    sceneView.autoenablesDefaultLighting = true
-    sceneView.backgroundColor = UIColor.black
-    
-//    ballNode = sceneView.scene?.rootNode.childNode(withName: "abstract_ball", recursively: true)
-    ballNode = sceneView.scene?.rootNode.childNodes[0]
-    ballNode?.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2 * .pi, z: 0, duration: 3)))
-    if ballNode == nil {
-      print("error: ballNode not found")
-    }
-    
-    return sceneView
+    let view = SCNView(frame: .zero)
+    view.scene = scene
+    view.autoenablesDefaultLighting = true
+    view.backgroundColor = UIColor.clear
+    view.sizeToFit()
+    return view
   }
   
   func updateUIView(_ uiView: SCNView, context: Context) {}
   
   func spinBall() {
     print("spinBall")
-    guard let ballNode = ballNode else { return }
-    print("ballNode OK")
+    let ballNode = scene.rootNode.childNode(withName: childNodeName, recursively: true)
     
     // Get a random direction and apply a random spin
     let direction = SCNVector3(x: Float.random(in: -1...1), y: Float.random(in: -1...1), z: Float.random(in: -1...1))
     let spin = SCNAction.repeatForever(SCNAction.rotateBy(x: CGFloat.random(in: -1...1), y: CGFloat.random(in: -1...1), z: CGFloat.random(in: -1...1), duration: 0.5))
     
     // Apply a force to the ball node to move it in the random direction
-    ballNode.physicsBody?.applyForce(direction, asImpulse: true)
+    ballNode?.physicsBody?.applyForce(direction, asImpulse: true)
     
     // Run the spin action for a short duration, then ease back to the original rotation
-    ballNode.runAction(spin, forKey: "spin")
-    ballNode.runAction(SCNAction.sequence([
+    ballNode?.runAction(spin, forKey: "spin")
+    ballNode?.runAction(SCNAction.sequence([
       SCNAction.wait(duration: 0.5),
       SCNAction.run { node in
         node.removeAction(forKey: "spin")
